cb23a0bcc3f35ba14bc2f7fe4655ce3f9c150f23
Mark Verrips <2083591@talnet.nl>
2020-02-13 13:36:32 +0100

    Uitwerking van de exercise

---
+++ wisselgeld.php
@@ -0,0 +1,16 @@
+<?php
+
+
+$input = $argv[1];
+echo intval($input). " x 1 euro";
+
+// if(!is_numeric($input)){
+//     echo "Geen wisselgeld";
+// }
+// elseif($input <= 0){
+//     echo "Geen wisselgeld";
+// }
+// else{
+// echo ($input . " x " . "1" . " euro");
+// }
+ ?>
